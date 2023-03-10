01389867e27691f1e205cf3f4220c3613592d6dc
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-03-10 01:43:28 +0100

    Refactor tests, add type tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,87 +1,105 @@
 import { describe, expect, it } from 'vitest';
 import { betterr, betterrSync } from '.';
+import { Expect, Equal } from '../tests/test-types';
 
-const returnData = async (data: string) => data;
-
-const returnDataSync = (data: string) => data;
-
-const throwError = async (message: string) => {
-  throw new Error(message);
-};
-
-const throwErrorSync = (message: string) => {
-  throw new Error(message);
-};
-
-const throwNonError = async (message: unknown) => {
-  throw message;
-};
-
-const throwNonErrorSync = (message: unknown) => {
-  throw message;
-};
+const message = 'message' as const;
 
 describe('betterr', () => {
   describe('Asynchronous functions', () => {
-    const data = 'data';
     it('When no error is thrown, data should exist and error should be null', async () => {
-      const { data: returnedData, err } = await betterr(() => returnData(data));
+      const { data, err } = await betterr(() => echo(message));
 
-      expect(returnedData).to.equal(data);
+      expect(data).to.equal(message);
       expect(err).toBe(null);
+
+      type BeforeReturn =
+        | Expect<Equal<typeof data, typeof message | null>>
+        | Expect<Equal<typeof err, Error | null>>;
+      if (err) return;
+      type AfterReturn =
+        | Expect<Equal<typeof data, typeof message>>
+        | Expect<Equal<typeof err, null>>;
     });
 
     it('When an asynchronous error is thrown, data should be null and error should exist', async () => {
-      const errorMessage = 'error';
-
-      const { data, err } = await betterr(() => throwError(errorMessage));
+      const { data, err } = await betterr(() => throwError(message));
 
       expect(data).toBe(null);
       expect(err).toBeInstanceOf(Error);
-      expect(err?.message).to.equal(errorMessage);
+      expect(err?.message).to.equal(message);
+
+      type BeforeReturn =
+        | Expect<Equal<typeof data, typeof message | null>>
+        | Expect<Equal<typeof err, Error | null>>;
+      if (err) return;
+      type AfterReturn =
+        | Expect<Equal<typeof data, typeof message>>
+        | Expect<Equal<typeof err, null>>;
     });
 
     it('When an asynchronous non-error is thrown, data should be null and error should exist', async () => {
-      const errorMessage = 'error';
+      const { data, err } = await betterr(() => throwNonError(message));
 
-      const { data, err } = await betterr(() => throwNonError(errorMessage));
       expect(data).toBe(null);
       expect(err).toBeInstanceOf(Error);
-      expect(err?.message).to.equal(errorMessage);
+      expect(err?.message).to.equal(message);
+
+      type BeforeReturn =
+        | Expect<Equal<typeof data, typeof message | null>>
+        | Expect<Equal<typeof err, Error | null>>;
+      if (err) return;
+      type AfterReturn =
+        | Expect<Equal<typeof data, typeof message>>
+        | Expect<Equal<typeof err, null>>;
     });
   });
 
   describe('Synchronous functions', () => {
     it('When no error is thrown, data should exist and error should be null', async () => {
-      const data = 'data';
+      const { data, err } = await betterr(() => echoSync(message));
 
-      const { data: returnedData, err } = await betterr(() =>
-        returnDataSync(data)
-      );
-
-      expect(returnedData).to.equal(data);
+      expect(data).to.equal(message);
       expect(err).toBe(null);
+
+      type BeforeReturn =
+        | Expect<Equal<typeof data, typeof message | null>>
+        | Expect<Equal<typeof err, Error | null>>;
+      if (err) return;
+      type AfterReturn =
+        | Expect<Equal<typeof data, typeof message>>
+        | Expect<Equal<typeof err, null>>;
     });
 
     it('When a synchronous error is thrown, data should be null and error should exist', async () => {
-      const errorMessage = 'error';
-
-      const { data, err } = await betterr(() => throwErrorSync(errorMessage));
+      const { data, err } = await betterr(() => throwErrorSync(message));
 
       expect(data).toBe(null);
       expect(err).toBeInstanceOf(Error);
-      expect(err?.message).to.equal(errorMessage);
+      expect(err?.message).to.equal(message);
+
+      type BeforeReturn =
+        | Expect<Equal<typeof data, typeof message | null>>
+        | Expect<Equal<typeof err, Error | null>>;
+      if (err) return;
+      type AfterReturn =
+        | Expect<Equal<typeof data, typeof message>>
+        | Expect<Equal<typeof err, null>>;
     });
 
     it('When a synchronous non-error is thrown, data should be null and error should exist', async () => {
-      const errorMessage = 'error';
+      const { data, err } = await betterr(() => throwNonErrorSync(message));
 
-      const { data, err } = await betterr(() =>
-        throwNonErrorSync(errorMessage)
-      );
       expect(data).toBe(null);
       expect(err).toBeInstanceOf(Error);
-      expect(err?.message).to.equal(errorMessage);
+      expect(err?.message).to.equal(message);
+
+      type BeforeReturn =
+        | Expect<Equal<typeof data, typeof message | null>>
+        | Expect<Equal<typeof err, Error | null>>;
+      if (err) return;
+      type AfterReturn =
+        | Expect<Equal<typeof data, typeof message>>
+        | Expect<Equal<typeof err, null>>;
     });
   });
 });
@@ -89,34 +107,78 @@ describe('betterr', () => {
 describe('betterrSync', () => {
   describe('Synchronous functions', () => {
     it('When no error is thrown, data should exist and error should be null', () => {
-      const data = 'data';
+      const { data, err } = betterrSync(() => echoSync(message));
 
-      const { data: returnedData, err } = betterrSync(() =>
-        returnDataSync(data)
-      );
-
-      expect(returnedData).to.equal(data);
+      expect(data).to.equal(message);
       expect(err).toBe(null);
+
+      type BeforeReturn =
+        | Expect<Equal<typeof data, typeof message | null>>
+        | Expect<Equal<typeof err, Error | null>>;
+      if (err) return;
+      type AfterReturn =
+        | Expect<Equal<typeof data, typeof message>>
+        | Expect<Equal<typeof err, null>>;
     });
 
     it('When a synchronous error is thrown, data should be null and error should exist', () => {
-      const errorMessage = 'error';
-
-      const { data, err } = betterrSync(() => throwErrorSync(errorMessage));
+      const { data, err } = betterrSync(() => throwErrorSync(message));
 
       expect(data).toBe(null);
       expect(err).toBeInstanceOf(Error);
-      expect(err?.message).to.equal(errorMessage);
+      expect(err?.message).to.equal(message);
+
+      type BeforeReturn =
+        | Expect<Equal<typeof data, typeof message | null>>
+        | Expect<Equal<typeof err, Error | null>>;
+      if (err) return;
+      type AfterReturn =
+        | Expect<Equal<typeof data, typeof message>>
+        | Expect<Equal<typeof err, null>>;
     });
 
     it('When a synchronous non-error is thrown, data should be null and error should exist', () => {
-      const errorMessage = 'error';
-
-      const { data, err } = betterrSync(() => throwNonErrorSync(errorMessage));
+      const { data, err } = betterrSync(() => throwNonErrorSync(message));
 
       expect(data).toBe(null);
       expect(err).toBeInstanceOf(Error);
-      expect(err?.message).to.equal(errorMessage);
+      expect(err?.message).to.equal(message);
+
+      type BeforeReturn =
+        | Expect<Equal<typeof data, typeof message | null>>
+        | Expect<Equal<typeof err, Error | null>>;
+      if (err) return;
+      type AfterReturn =
+        | Expect<Equal<typeof data, typeof message>>
+        | Expect<Equal<typeof err, null>>;
     });
   });
 });
+
+async function echo<T>(message: T) {
+  return message;
+}
+
+function echoSync<T>(message: T) {
+  return message;
+}
+
+async function throwError<T extends string>(message: T) {
+  throw new Error(message);
+  return message;
+}
+
+function throwErrorSync<T extends string>(message: T) {
+  throw new Error(message);
+  return message;
+}
+
+async function throwNonError<T>(message: T) {
+  throw message;
+  return message;
+}
+
+function throwNonErrorSync<T>(message: T) {
+  throw message;
+  return message;
+}
