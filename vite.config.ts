5343b1746558d4a16992b131d897e596252d039f
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-03-13 02:10:52 +0100

    Make use of process.env.CI = true

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
-import { CoverageReporter } from 'vitest';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     coverage: {
-      reporter: getReporter(process.env.NODE_ENV),
+      reporter: process.env.CI ? ['text', 'json-summary'] : ['text', 'html'],
     },
   },
 });
-
-function getReporter(environment: string | undefined): CoverageReporter[] {
-  switch (environment) {
-    case 'CI':
-      return ['text', 'json-summary'];
-    default:
-      return ['text', 'html'];
-  }
-}
