da92c29bb9dc792dd831f46318be86ac90756aea
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-03-14 12:17:17 +0100

    Temp - restore passing commit to find error cause

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['text', 'html', 'json-summary'],
+      reporter: process.env.CI ? ['text', 'json-summary'] : ['text', 'html'],
     },
   },
 });
